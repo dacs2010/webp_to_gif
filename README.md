cc82bc24a12c510a961b9c7ec97dbf050576bb05
dacs2010 <dacs2010@gmail.com>
2022-08-22 11:21:53 -0700

    updates README, adds code to check for animation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # webp_to_gif
 will convert all files in a directory with the webp extension to gifs
+
+## features roadmap
+
+- check for animation
+- print files to be converted, then check user input to continue or cancel
+- replace webp with gif option
--- webp_to_gif.py
+++ webp_to_gif.py
@@ -1,9 +1,5 @@
 ''' 
-Get all files in a directory with the webp ext and convert them to gif.
-
-This currently works, however it will convert ANY webp to gif. The next step is to loop through and test to see if the webp is animated or not, if so then convert it.
-
-A possible next step is to remove the original image and replace it with the new file, however this might be bad practice.
+Converts files in the directory with extention webp to gif
 '''
 
 #import os
@@ -18,6 +14,8 @@ print(files)
 for file in files:
   im = Image.open(file)
   print(file)
-  new_name = file.replace('.webp','')
-  print(new_name)
-  im.save(str(new_name)+'.gif', 'gif', save_all=True, optimize=True, background=0)
+  if im.is_animated == True:
+    new_name = file.replace('.webp','.gif')
+    print(new_name)
+    #im.save(str(new_name)+'.gif', 'gif', save_all=True, optimize=True, background=0)
+  else: print(file + ' is not an animation')
